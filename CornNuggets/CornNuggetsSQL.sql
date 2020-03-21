f628eeacf584517d7fb0cfd60b97cd747a97273a
staceyloulouse <staceyloulouse@gmail.com>
2020-03-21 12:39:09 -0400

    edited sql query for project 0

--- CornNuggets/CornNuggetsSQL.sql
+++ CornNuggets/CornNuggetsSQL.sql
@@ -13,7 +13,7 @@ CREATE TABLE Products
 (
     ProductID INT primary key,
     ProductName NVARCHAR(50),
-    Price money,
+    ProductPrice money default 0.00,
     Inventory INT
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE Customers
     CustomerID INT primary key identity(100000000, 1),
     FirstName NVARCHAR(50),
     LastName NVARCHAR(50),
-    PreferredStore VARCHAR(8) FOREIGN KEY REFERENCES NuggetStores(StoreName)  
+    PreferredStore VARCHAR(7) FOREIGN KEY REFERENCES NuggetStores(StoreName)  
 );
 
 
@@ -35,35 +35,40 @@ CREATE TABLE Orders
 (
     OrderID INT primary key identity(1000, 1),
     DateTimeStamp DATETIME,
-    StoreID INT, --may need to link to store ID using foreign key
-    CustomerID INT, --may need to link to customer ID using foreign key
+    StoreID INT Foreign Key References NuggetStores(StoreID), --may need to link to store ID using foreign key
+    CustomerID INT Foreign Key References Customers(CustomerID), --may need to link to customer ID using foreign key
     Total money
 );
 
 
 --insert into customer; set first name and last name of demo customer
-INSERT INTO Customers values ('Derrell', 'Brown', 'SCAR001')
+INSERT INTO Customers values ('Derrell', 'Brown', 'SCAR001');
+INSERT INTO Customers values ('Stacey', 'Joseph', 'TEXAS01');
+INSERT INTO Customers values ('Eunique', 'Joseph', 'TEXAS02');
+INSERT INTO Customers values ('Euri', 'Joseph', 'TEXAS02');
+INSERT INTO Customers values ('Euriah', 'Joseph', 'TEXAS02');
+INSERT INTO Customers values ('Astrid', 'Ryan', 'SCAR001');
 
 
 --insert into NuggetStores; names and location of all stores
-INSERT INTO NuggetStores values ('TEXAS001', 'Arlington')
-INSERT INTO NuggetStores values ('TEXAS002', 'Dallas')
-INSERT INTO NuggetStores values ('TEXAS003', 'Houston')
-INSERT INTO NuggetStores values ('TEXAS004', 'Arlington')
-INSERT INTO NuggetStores values ('FLORI001', 'Miami')
-INSERT INTO NuggetStores values ('FLORI002', 'Daytona Beach')
-INSERT INTO NuggetStores values ('FLORI003', 'Jacksonville')
-INSERT INTO NuggetStores values ('FLORI004', 'Tampa')
-INSERT INTO NuggetStores values ('GEORG001', 'Augusta')
-INSERT INTO NuggetStores values ('GEORG002', 'Atlanta')
-INSERT INTO NuggetStores values ('GEORG003', 'Albany')
-INSERT INTO NuggetStores values ('GEORG004', 'Hephzibah')
-INSERT INTO NuggetStores values ('NEWYO001', 'Brooklyn')
-INSERT INTO NuggetStores values ('UTAH0001', 'Salt Lake City')
-INSERT INTO NuggetStores values ('CALIF001', 'Los Angelos')
-INSERT INTO NuggetStores values ('CALIF002', 'San Francisco')
-INSERT INTO NuggetStores values ('CALIF003', 'Sacramento')
-INSERT INTO NuggetStores values ('SCARO001', 'Columbia')
+INSERT INTO NuggetStores values ('TEXAS01', 'Arlington')
+INSERT INTO NuggetStores values ('TEXAS02', 'Dallas')
+INSERT INTO NuggetStores values ('TEXAS03', 'Houston')
+INSERT INTO NuggetStores values ('TEXAS04', 'Arlington')
+INSERT INTO NuggetStores values ('FLOR001', 'Miami')
+INSERT INTO NuggetStores values ('FLOR002', 'Daytona Beach')
+INSERT INTO NuggetStores values ('FLOR003', 'Jacksonville')
+INSERT INTO NuggetStores values ('FLOR004', 'Tampa')
+INSERT INTO NuggetStores values ('GEOR001', 'Augusta')
+INSERT INTO NuggetStores values ('GEOR002', 'Atlanta')
+INSERT INTO NuggetStores values ('GEOR003', 'Albany')
+INSERT INTO NuggetStores values ('GEOR004', 'Hephzibah')
+INSERT INTO NuggetStores values ('NYOR001', 'Brooklyn')
+INSERT INTO NuggetStores values ('UTAH001', 'Salt Lake City')
+INSERT INTO NuggetStores values ('CALI001', 'Los Angelos')
+INSERT INTO NuggetStores values ('CALI002', 'San Francisco')
+INSERT INTO NuggetStores values ('CALI003', 'Sacramento')
+INSERT INTO NuggetStores values ('SCAR001', 'Columbia')
 
 
 --insert into product the inventory of items available for order
@@ -72,13 +77,13 @@ INSERT INTO Products values (112, 'Nacho', 4.5, 1000)
 INSERT INTO Products values (113, 'Blue', 3.5, 1000)
 INSERT INTO Products values (114, 'Tomato', 4.0, 1000)
 INSERT INTO Products values (222, 'Salsa', 1.5, 1000)
-INSERT INTO Products values (223, 'Avocado', Price, 1000)
-INSERT INTO Products values (224, 'Onion', Price, 1000)
-INSERT INTO Products values (225, 'Cheese', Price, 1000)
-INSERT INTO Products values (333, 'Fizzy', Price, 1000)
-INSERT INTO Products values (334, 'Tea', Price, 1000)
-INSERT INTO Products values (335, 'Juice', Price, 1000)
-INSERT INTO Products values (336, 'Smoothie', Price, 1000)
+INSERT INTO Products values (223, 'Avocado', 2.0, 1000)
+INSERT INTO Products values (224, 'Onion', 1.0, 1000)
+INSERT INTO Products values (225, 'Cheese', 2.5, 1000)
+INSERT INTO Products values (333, 'Fizzy', 1.5, 1000)
+INSERT INTO Products values (334, 'Tea', 1.00, 1000)
+INSERT INTO Products values (335, 'Juice', 1.75, 1000)
+INSERT INTO Products values (336, 'Smoothie', 2.5, 1)
 
 
 --create an order log table with products in a particular order; has an order id, product id, and quantity
@@ -88,6 +93,17 @@ CREATE TABLE OrderLog
     ProductID INT FOREIGN KEY REFERENCES Products(ProductID), --link to a product in the table as a foreign key
     ProductQty INT
 );
+
+--show all stores, customers and products in their respective tables
+select *
+from customers;
+
+select * 
+from nuggetstores;
+
+select *
+from products;
+
 
 --select orders by store
 SELECT *
@@ -115,3 +131,8 @@ FROM Customers;
 SELECT *
 FROM OrderLog
 GROUP BY OrderID;
+
+update Customers
+set PreferredStore = 'TEXA002'
+from customers
+where LastName = 'Joseph';
